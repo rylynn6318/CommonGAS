36f7abfedab8246e6ccbccb16f73d26195f5a7aa
rylynn6318 <gg6318@naver.com>
2026-02-26 22:18:23 +0900

    Initial commit

---
+++ Source/CommonGAS/Private/CommonGameplayAbility.cpp
@@ -0,0 +1,66 @@
+﻿// Copyright 2026 rylynn6318. All rights reserved.
+
+
+#include "CommonGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+
+const FGameplayTagContainer* UCommonGameplayAbility::GetCooldownTags() const
+{
+	FGameplayTagContainer* MutableGameplayTags = const_cast<FGameplayTagContainer*>(&TempCooldownTags);
+	MutableGameplayTags->Reset();
+
+	const FGameplayTagContainer* ParentTags = Super::GetCooldownTags();
+	if (ParentTags)
+	{
+		MutableGameplayTags->AppendTags(*ParentTags);
+	}
+
+	MutableGameplayTags->AppendTags(CooldownTags);
+	return MutableGameplayTags;
+}
+
+void UCommonGameplayAbility::ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	if (UGameplayEffect* CooldownGameplayEffect = GetCooldownGameplayEffect())
+	{
+		FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(CooldownGameplayEffect->GetClass(), GetAbilityLevel());
+		SpecHandle.Data.Get()->DynamicGrantedTags.AppendTags(CooldownTags);
+		SpecHandle.Data.Get()->SetSetByCallerMagnitude(CooldownTags.GetByIndex(0), CooldownDuration.GetValueAtLevel(GetAbilityLevel()));
+		ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, SpecHandle);
+	}
+}
+
+APlayerController* UCommonGameplayAbility::GetPlayerControllerFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<APlayerController>(CurrentActorInfo->PlayerController.Get()) : nullptr);
+}
+
+AController* UCommonGameplayAbility::GetControllerFromActorInfo() const
+{
+	if (CurrentActorInfo)
+	{
+		if (AController* PC = CurrentActorInfo->PlayerController.Get())
+		{
+			return PC;
+		}
+
+		// Look for a player controller or pawn in the owner chain.
+		AActor* TestActor = CurrentActorInfo->OwnerActor.Get();
+		while (TestActor)
+		{
+			if (AController* C = Cast<AController>(TestActor))
+			{
+				return C;
+			}
+
+			if (const APawn* Pawn = Cast<APawn>(TestActor))
+			{
+				return Pawn->GetController();
+			}
+
+			TestActor = TestActor->GetOwner();
+		}
+	}
+
+	return nullptr;
+}
---
+++ Source/CommonGAS/Public/CommonGameplayAbility.h
@@ -0,0 +1,41 @@
+﻿// Copyright 2026 rylynn6318. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CommonGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMONGAS_API UCommonGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	// Control Cooldown via SetByCaller
+	virtual const FGameplayTagContainer* GetCooldownTags() const override;
+	virtual void ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+	// ~SetByCaller
+	
+	UFUNCTION(BlueprintCallable, Category = "Ability")
+	APlayerController* GetPlayerControllerFromActorInfo() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Ability")
+	AController* GetControllerFromActorInfo() const;
+
+protected:
+	// 쿨다운 태그, 태그 별로 한개의 쿨타임만 적용됨.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability|Cooldown", meta = (Categories = "SetByCaller"))
+	FGameplayTagContainer CooldownTags;
+
+	// 쿨다운 지속시간
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability|Cooldown")
+	FScalableFloat CooldownDuration;
+
+private:
+	UPROPERTY(Transient)
+	FGameplayTagContainer TempCooldownTags;
+};
